afae49828f728f8e116569ca5551ab53434b1e15
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-04-07 00:16:03 +0700

    Trick type and error handling

--- my-app/src/pages/AddStudent/AddStudent.tsx
+++ my-app/src/pages/AddStudent/AddStudent.tsx
@@ -3,7 +3,8 @@ import { useMutation } from 'react-query'
 import http from '../../utils/http'
 import { addStudent } from '../../apis/students.api'
 import { Student } from '../../types/students.type'
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
+import { isAxiosError } from '../../utils/utils'
 
 type FormStateType = Omit<Student, 'id'>
 const initialFormState: FormStateType = {
@@ -15,35 +16,64 @@ const initialFormState: FormStateType = {
   avatar: '',
   btc_address: ''
 }
+
+type FormError =
+  | {
+      [key in keyof FormStateType]: string
+    }
+  | null
 export default function AddStudent() {
   const [formState, setFormState] = useState<FormStateType>(initialFormState)
 
   const addMatch = useMatch('/students/add')
   const isAddMode = Boolean(addMatch)
 
-  const { mutate } = useMutation({
+  const { mutate, error, data, reset, mutateAsync } = useMutation({
     mutationFn: (body: FormStateType) => {
       return addStudent(body)
     }
   })
+  // sd useMemo de han che viec tinh toan di tinh toan lai
+  const errorForm: FormError = useMemo(() => {
+    if (isAxiosError<{ error: FormError }>(error) && error.response?.status === 422) {
+      return error.response?.data.error
+    }
+    return null
+  }, [error])
 
   //Dung currying
   const handleChange = (name: keyof FormStateType) => (event: React.ChangeEvent<HTMLInputElement>) => {
     setFormState((prev) => ({ ...prev, [name]: event.target.value }))
+    if (data || error) {
+      reset()
+    }
   }
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    console.log(formState)
-    mutate(formState)
+    try {
+      const data = await mutateAsync(formState)
+      setFormState(initialFormState)
+      console.log(data, 'data')
+    } catch (error) {
+      console.log(error, 'err')
+    }
   }
+  // const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault()
+  //   mutate(formState, {
+  //     onSuccess: () => {
+  //       setFormState(initialFormState)
+  //     }
+  //   })
+  // }
   return (
     <div>
       <h1 className='text-lg font-medium'>{isAddMode ? 'Add Student' : 'Edit Student'}</h1>
       <form className='mt-6' onSubmit={handleSubmit}>
         <div className='group relative z-0 mb-6 w-full'>
           <input
-            type='email'
+            type=''
             name='floating_email'
             id='floating_email'
             className='peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500'
@@ -58,6 +88,7 @@ export default function AddStudent() {
           >
             Email address
           </label>
+          {errorForm && <p className='mt-2 text-sm text-red-600'>{errorForm.email}</p>}
         </div>
 
         <div className='group relative z-0 mb-6 w-full'>
--- my-app/src/utils/utils.ts
+++ my-app/src/utils/utils.ts
@@ -1,6 +1,12 @@
 import { useSearchParams } from 'react-router-dom'
+import axios, { AxiosError } from 'axios'
+
 export const useQueryString = () => {
   const [searchParams] = useSearchParams()
   const searchParamsObject = Object.fromEntries([...searchParams])
   return searchParamsObject
+}
+
+export function isAxiosError<T>(error: unknown): error is AxiosError<T> {
+  return axios.isAxiosError(error)
 }
